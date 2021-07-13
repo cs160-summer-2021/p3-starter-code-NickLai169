28be8b979e7095542aeb6dd231103d3d23c1936a
Zhijia Deng <dengzhijia@gmail.com>
2021-07-13 16:25:37 -0700

    finished homepage

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('new_interaction', views.index, name='new_interaction'),
+    path('home', views.home, name='home'),
+    path('pages', views.pages, name='pages'),
+    path('npages', views.npages, name='npages'),
+
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+def home(request):
+    return render(request, 'coloring/home.html')
+def pages(request):
+    return render(request, 'coloring/pages.html')
+def npages(request):
+    return render(request, 'coloring/npages.html')
