4d50235a9c5ce2ac2b4c81ddab6e28ca286053bb
Yiming Wang <w448415029w@gmail.com>
2021-07-14 20:51:52 -0700

    finished project

--- coloring/urls.py
+++ coloring/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('demo', views.index, name='demo'),
     path('new_interaction', views.index, name='new_interaction'),
+    path('canvas', views.canvas, name='canvas'),
     path('home', views.home, name='home'),
     path('pages', views.pages, name='pages'),
     path('npages', views.npages, name='npages'),
     path('addedpages', views.addedpages, name='addedpages'),
-
 ]
